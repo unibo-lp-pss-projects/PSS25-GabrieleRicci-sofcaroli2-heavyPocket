318fffafa60dda2f2b6594992809d6521863ebea
sofcaroli2 <144837063+sofcaroli2@users.noreply.github.com>
2026-03-11 15:51:30 +0100

    creating StatisticsController interface and class

---
+++ src/main/java/it/unibo/heavypocket/mvc/controller/StatisticsController.java
@@ -0,0 +1,5 @@
+package it.unibo.heavypocket.mvc.controller;
+
+public interface StatisticsController {
+
+}
---
+++ src/main/java/it/unibo/heavypocket/mvc/controller/impl/StatisticsControllerImpl.java
@@ -0,0 +1,7 @@
+package it.unibo.heavypocket.mvc.controller.impl;
+
+import it.unibo.heavypocket.mvc.controller.StatisticsController;
+
+public class StatisticsControllerImpl implements StatisticsController {
+
+}
