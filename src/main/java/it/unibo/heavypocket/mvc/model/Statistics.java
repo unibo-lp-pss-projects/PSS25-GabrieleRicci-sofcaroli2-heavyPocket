f68125bb6b0bd65699de72acc512d5c4eb43a481
sofcaroli2 <144837063+sofcaroli2@users.noreply.github.com>
2026-03-11 15:37:21 +0100

    adding Statistics methods

--- src/main/java/it/unibo/heavypocket/mvc/model/Statistics.java
+++ src/main/java/it/unibo/heavypocket/mvc/model/Statistics.java
@@ -1,6 +1,7 @@
 package it.unibo.heavypocket.mvc.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Map;
 
 public interface Statistics {
@@ -8,9 +9,10 @@ public interface Statistics {
     // sotto il conto --> media totale delle uscite
     BigDecimal getAverageExpense();
 
-    // piechart --> soldi spesi divisi per categoria: con tag invece di string per passare anche il colore
+    // piechart --> soldi spesi divisi per categoria: con tag invece di string per
+    // passare anche il colore
     Map<Tag, BigDecimal> getExpenseByTag();
 
     // line chart --> balance diviso per mese alla fine del mese
-    Map<String, BigDecimal> getStatisticsByMonth();
+    Map<LocalDate, BigDecimal> getStatisticsByMonth();
 }
--- src/main/java/it/unibo/heavypocket/mvc/model/impl/StatisticsImpl.java
+++ src/main/java/it/unibo/heavypocket/mvc/model/impl/StatisticsImpl.java
@@ -3,11 +3,13 @@ package it.unibo.heavypocket.mvc.model.impl;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.stream.Collectors;
 
+import it.unibo.heavypocket.mvc.model.Tag;
 import it.unibo.heavypocket.mvc.model.Statistics;
 import it.unibo.heavypocket.mvc.model.Transaction;
-import it.unibo.heavypocket.mvc.model.Tag;
-import java.math.RoundingMode;
 
 public final class StatisticsImpl implements Statistics {
 
@@ -18,27 +20,39 @@ public final class StatisticsImpl implements Statistics {
     public StatisticsImpl(final List<Transaction> transactions) {
         this.transactions = transactions;
         this.expenses = transactions.stream().filter(Transaction::isExpense).toList();
-        this.expensesCount = new BigDecimal(expenses.size());
+        this.expensesCount = new BigDecimal(this.expenses.size());
     }
 
     @Override
     public BigDecimal getAverageExpense() {
-        final BigDecimal AverageExpense = expenses.stream()
+        final BigDecimal averageExpense = expenses.stream()
                 .map(Transaction::getAmount) // so già che sono tutte negative, quindi non serve signedAmout
                 .reduce(BigDecimal.ZERO, BigDecimal::add) // somma tutti i valori insieme
                 .divide(expensesCount, 2, RoundingMode.HALF_UP); // dividi per il numero di spese per ottenere la media
-        return AverageExpense;
-    }  
-    
-    // @Override
-    // public Map<Tag, BigDecimal> getExpenseByTag() {
-    //     return null;
-    // }     
-    
-    // @Override           
-    // public Map<String, BigDecimal> getStatisticsByMonth() {
-    //     return null;
-    // }
+        return averageExpense;
+    }
 
-    
+    @Override
+    public Map<Tag, BigDecimal> getExpenseByTag() {
+        final Map<Tag, BigDecimal> expenseByTag = expenses.stream()
+                .collect(Collectors.toMap(
+                        Transaction::getTag,
+                        Transaction::getAmount,
+                        BigDecimal::add));
+        return expenseByTag;
+    }
+
+    @Override
+    public Map<LocalDate, BigDecimal> getStatisticsByMonth() {
+        final LocalDate todayDate = LocalDate.now(); // data di oggi per sapere il mese corrente
+        final Map<LocalDate, BigDecimal> statisticsByMonth = transactions.stream()
+                .filter(t -> t.getDate().getMonth() == todayDate.getMonth()
+                        && t.getDate().getYear() == todayDate.getYear())
+                // filtra solo le tranzazioni che sono nle mese e anno della data corrente;
+                .collect(Collectors.toMap(
+                        Transaction::getDate,
+                        Transaction::getAmount,
+                        BigDecimal::add));
+        return statisticsByMonth;
+    }
 }
