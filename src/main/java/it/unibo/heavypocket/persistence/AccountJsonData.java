7f93d95871b409d0c868dd10cc8afbe6f24f5b78
Anna Passini <166128792+Anapaxx@users.noreply.github.com>
2026-03-19 12:12:10 +0100

    removed comments

--- src/main/java/it/unibo/heavypocket/persistence/AccountJsonData.java
+++ src/main/java/it/unibo/heavypocket/persistence/AccountJsonData.java
@@ -3,13 +3,10 @@ package it.unibo.heavypocket.persistence;
 import java.math.BigDecimal;
 import java.util.List;
 
-//@TODO: nel json dovrebbero andarci tutte le cose che non vengono calcolate da sole, 
-// quindi balance non dovrebbe venire salvato in quanto calcolato da AccountImpl
 /**
  * Account payload used for JSON persistence.
  * 
  * @param transactions serialized transactions list
- * @param balance      persisted account balance
  * @param budget       persisted budget limit
  */
 public record AccountJsonData(
