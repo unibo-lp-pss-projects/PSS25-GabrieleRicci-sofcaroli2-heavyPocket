6e2033abefb1dd9cdcca7150e0bde21e7627d172
sofcaroli2 <144837063+sofcaroli2@users.noreply.github.com>
2026-03-19 15:30:54 +0100

    hadCode() and equals() in TransactionImpl

--- src/main/java/it/unibo/heavypocket/mvc/model/impl/TransactionImpl.java
+++ src/main/java/it/unibo/heavypocket/mvc/model/impl/TransactionImpl.java
@@ -8,6 +8,7 @@ import static java.util.Objects.requireNonNull;
 import it.unibo.heavypocket.mvc.model.Transaction;
 import it.unibo.heavypocket.mvc.model.TransactionType;
 import it.unibo.heavypocket.mvc.model.Tag;
+import java.util.Objects;
 
 /**
  * Implementation of the Transaction interface. It includes amount, date,
@@ -103,4 +104,30 @@ public final class TransactionImpl implements Transaction {
     public Tag getTag() {
         return tag;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TransactionImpl other = (TransactionImpl) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
 }
