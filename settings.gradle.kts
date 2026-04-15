89e99bbb315cf5b6dca7f6c5a460712cb1d0d0c1
Danilo Pianini <danilo.pianini@unibo.it>
2026-04-15 16:44:10 +0200

    fix the project name in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "pss-25-sofcaroli2/heavyPocket"
+rootProject.name = "heavyPocket"
 plugins {
     id("com.gradle.develocity") version "4.3.2"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
